103e0ec32cedfcda70a5ced0bd3d7c6a9c7a87b0
Aliza5656 <alizayousuf56@gmail.com>
2025-07-19 23:44:23 +0500

    change

---
+++ home.php
@@ -0,0 +1,35 @@
+<div>
+<?php
+
+echo "<h1>Multiplication Table</h1>";
+
+
+
+$count = 15;
+
+
+for($i = 1; $i <= 10 ; $i++){
+
+
+    echo "<div class='table'>" . $count . " x " . $i . " = <span>" . ($i * $count) . "</span><br></div>";
+
+}
+
+
+
+
+?>
+</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP First Code</title>
+
+</head>
+<body>
+  
+</body>
+</html>
